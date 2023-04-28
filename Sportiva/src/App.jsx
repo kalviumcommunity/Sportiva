73a7c0e9059cdbbe3178d0a75f351564fa6cc756
Harish <harishnik08@gmail.com>
2023-04-28 18:10:48 +0530

    made some changes

--- Sportiva/src/App.jsx
+++ Sportiva/src/App.jsx
@@ -3,18 +3,19 @@ import StudentsCard from "./pages/StudentsListing/StudentsCard";
 import { ChakraProvider } from "@chakra-ui/react";
 import "./index.css";
 import theme from "./chakraTheme.jsx";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import Homepage from "./pages/Homepage/HomePage";
+// import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+// import Homepage from "./pages/Homepage/HomePage";
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Router>
+      {/* <Router>
         <Routes>
-          <Route exact path="/" component={Homepage} />
-          <Route path="/StudentsListing" component={StudentsCard} />
+        <Route exact path="/" element={<Homepage />} />
+        <Route path="/studentsListingPage" element={<StudentsCard />} />
         </Routes>
-      </Router>
+      </Router> */}
+      <StudentsCard/>
     </ChakraProvider>
   );
 }
