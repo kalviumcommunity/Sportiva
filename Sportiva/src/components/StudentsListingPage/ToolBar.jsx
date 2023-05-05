c1ad9e928482ca9a02f65f5def327dcc24f2b1e1
Harish <harishnik08@gmail.com>
2023-05-05 19:35:16 +0530

    changes made

--- Sportiva/src/components/StudentsListingPage/ToolBar.jsx
+++ Sportiva/src/components/StudentsListingPage/ToolBar.jsx
@@ -1,15 +1,14 @@
 import { Input, Image, Box, Flex, Divider } from "@chakra-ui/react";
 import NavBar from "./NavBar";
+import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-// eslint-disable-next-line react/prop-types
-export default function ToolBar({handleSearch}) {
+function ToolBar({ handleSearch }) {
   return (
-  
-      <Box px="94px">
-        <NavBar />
-        <Flex alignItems="flex-end"  justifyContent="space-between">
-          <Link to="/student-form">
+    <Box px="94px">
+      <NavBar />
+      <Flex alignItems="flex-end" justifyContent="space-between">
+        <Link to="/student-form">
           <Image
             src="/images/AddStudent.svg"
             alt="img"
@@ -17,21 +16,26 @@ export default function ToolBar({handleSearch}) {
             mt="110px"
             mr="1000px"
           />
-          </Link>
-          <Input
-            placeholder="Find students"
-            h="45px"
-            w="628px"
-            border="1px"
-            borderColor="black"
-            borderRadius="4px"
-            mt="103px"
-            fontWeight="regular"
-            onChange={handleSearch}
-          />
-        </Flex>
-        <Divider borderColor="#E3E3E3" mt="29" />
-      </Box>
-    
+        </Link>
+        <Input
+          placeholder="Find students"
+          h="45px"
+          w="628px"
+          border="1px"
+          borderColor="black"
+          borderRadius="4px"
+          mt="103px"
+          fontWeight="regular"
+          onChange={handleSearch}
+        />
+      </Flex>
+      <Divider borderColor="#E3E3E3" mt="29" />
+    </Box>
   );
 }
+
+ToolBar.propTypes = {
+  handleSearch: PropTypes.func.isRequired,
+};
+
+export default ToolBar;
