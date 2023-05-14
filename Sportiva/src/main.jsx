2066ded8de4edb7fca994bb678fd06a9cded7ac7
Harish <harishnik08@gmail.com>
2023-05-14 11:23:01 +0530

    changes made

--- Sportiva/src/main.jsx
+++ Sportiva/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import {Auth0Provider} from '@auth0/auth0-react';
+import Auth0Provider from "./auth/auth0-provider-with-history";
 
 const domain = import.meta.env.SPORTIVA_AUTH0_DOMAIN;
 const clientId = import.meta.env.SPORTIVA_AUTH0_CLIENT_ID;
