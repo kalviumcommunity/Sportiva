c5b70093384598fe46e1f5053600bc7390bba50d
Harish <harishnik08@gmail.com>
2023-05-14 14:45:15 +0530

    deleted Auth files

--- Sportiva/src/main.jsx
+++ Sportiva/src/main.jsx
@@ -2,19 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import Auth0Provider from "./auth/auth0-provider-with-history";
 
-const domain = import.meta.env.SPORTIVA_AUTH0_DOMAIN;
-const clientId = import.meta.env.SPORTIVA_AUTH0_CLIENT_ID;
-
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}
-  >
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </Auth0Provider>
-);
+  </React.StrictMode>,
+)
