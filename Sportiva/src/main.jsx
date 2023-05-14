847dae4bd40d481749cfa4ec2accdacff952e8f8
Harish <harishnik08@gmail.com>
2023-05-14 11:39:48 +0530

    changes made

--- Sportiva/src/main.jsx
+++ Sportiva/src/main.jsx
@@ -2,9 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import Auth0Provider from "./auth/auth0-provider-with-history";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}>
     <App />
-  </React.StrictMode>,
+  </Auth0Provider>,
 )
