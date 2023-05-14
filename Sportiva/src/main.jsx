3e6266fd9a9c2859d56ffb1c3bd899aa5fcac6b5
Harish <harishnik08@gmail.com>
2023-05-14 12:25:14 +0530

    changes made

--- Sportiva/src/main.jsx
+++ Sportiva/src/main.jsx
@@ -1,16 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import Auth0Provider from "./auth/auth0-provider-with-history";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import Auth0Provider from "./auth0/auth0-provider-with-history.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <Auth0Provider
-    domain={domain}
-    clientId={clientId}
-    authorizationParams={{
-      redirect_uri: window.location.origin,
-    }}>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Auth0Provider>
     <App />
-  </Auth0Provider>,
-)
+  </Auth0Provider>
+);
