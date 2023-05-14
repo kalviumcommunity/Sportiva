bb297724f5e6f0a224faf67e59ccf495b0c1de9c
Harish <harishnik08@gmail.com>
2023-05-14 10:48:57 +0530

    auth0 imported and worked on

--- Sportiva/src/main.jsx
+++ Sportiva/src/main.jsx
@@ -2,9 +2,19 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {Auth0Provider} from '@auth0/auth0-react';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
+const domain = import.meta.env.SPORTIVA_AUTH0_DOMAIN;
+const clientId = import.meta.env.SPORTIVA_AUTH0_CLIENT_ID;
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <Auth0Provider
+    domain={domain}
+    clientId={clientId}
+    authorizationParams={{
+      redirect_uri: window.location.origin,
+    }}
+  >
     <App />
-  </React.StrictMode>,
-)
+  </Auth0Provider>
+);
--- Sportiva/src/pages/Homepage/HomePage.jsx
+++ Sportiva/src/pages/Homepage/HomePage.jsx
@@ -1,31 +1,35 @@
 import "./Homepage.css";
 import { Link } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
 
 export default function Homepage() {
+  const {loginWithRedirect} = useAuth0()
   return (
-          <div className="main-div">
-        <div className="main">
-          <div className="left-div">
-            <h1>SPORTIVA</h1>
-            <p className="Note1">Unleash the inner champion</p>
-            <p className="Note2">
-              The ultimate tool for tracking and achieving your athletic goals!
-            </p>
-            <Link to ="/students-listing">
-              <button className="start-button">Start Tracking</button>
-            </Link>
-            <button className="login-button">Login</button>
-          </div>
-          <div className="right-div">
-            <img
-              className="home-page-img"
-              src="http://www.worldtaekwondo.org/wp-content/uploads/2018/12/DEM_0046.jpg"
-              alt="img"
-              height="828px"
-              width="1190px"
-            />
-          </div>
+    <div className="main-div">
+      <div className="main">
+        <div className="left-div">
+          <h1>SPORTIVA</h1>
+          <p className="Note1">Unleash the inner champion</p>
+          <p className="Note2">
+            The ultimate tool for tracking and achieving your athletic goals!
+          </p>
+          <Link to="/students-listing">
+            <button className="start-button">Start Tracking</button>
+          </Link>
+          <button className="login-button" onClick={() => loginWithRedirect()}>
+            Login
+          </button>
+        </div>
+        <div className="right-div">
+          <img
+            className="home-page-img"
+            src="http://www.worldtaekwondo.org/wp-content/uploads/2018/12/DEM_0046.jpg"
+            alt="img"
+            height="828px"
+            width="1190px"
+          />
         </div>
       </div>
-      );
+    </div>
+  );
 }
