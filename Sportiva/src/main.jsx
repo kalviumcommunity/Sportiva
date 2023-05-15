343dc65bda5f418918f7abe6ead663da2632945b
Harish <harishnik08@gmail.com>
2023-05-15 09:09:58 +0530

    changes made

--- Sportiva/src/main.jsx
+++ Sportiva/src/main.jsx
@@ -1,13 +1,10 @@
-import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import Auth0Provider from './auth0/auth0-provider-with-history'
-
+import ReactDOM from "react-dom/client";
+import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-        <BrowserRouter>
-            <App />
             <Auth0Provider>
                 <App />
             </Auth0Provider>
-        </BrowserRouter>
 );
