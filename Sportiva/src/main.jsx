26ea07ac3a842bd0b1031c76ec78046ef845f6a6
Harish <harishnik08@gmail.com>
2023-05-15 11:47:50 +0530

    changes made

--- Sportiva/src/main.jsx
+++ Sportiva/src/main.jsx
@@ -1,5 +1,4 @@
 import './index.css'
-// import Auth0Provider from './auth0/auth0-provider-with-history'
 import ReactDOM from "react-dom/client";
 import App from './App';
 import { Auth0Provider } from '@auth0/auth0-react';
