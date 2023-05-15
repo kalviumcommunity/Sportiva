ca20bbd8353eb0039a5849b0167230cfc55c7d54
Harish <harishnik08@gmail.com>
2023-05-15 11:28:22 +0530

    changes made used logout in studentsListingPage

--- Sportiva/src/main.jsx
+++ Sportiva/src/main.jsx
@@ -1,10 +1,19 @@
 import './index.css'
-import Auth0Provider from './auth0/auth0-provider-with-history'
+// import Auth0Provider from './auth0/auth0-provider-with-history'
 import ReactDOM from "react-dom/client";
 import App from './App';
+import { Auth0Provider } from '@auth0/auth0-react';
 
+const domain = import.meta.env.VITE_AUTH0_DOMAIN;
+const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
+console.log(domain,clientId)
 ReactDOM.createRoot(document.getElementById('root')).render(
-            <Auth0Provider>
+            <Auth0Provider
+            domain={domain}
+            clientId={clientId}
+            authoriztionParams = {{
+                redirect_uri : window.location.origin
+            }}>
                 <App />
             </Auth0Provider>
 );
