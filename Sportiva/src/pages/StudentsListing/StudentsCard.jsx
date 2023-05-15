1df29b50d9597686e15f95e29a0301325d1112cc
Harish <harishnik08@gmail.com>
2023-05-15 14:02:21 +0530

    changes made

--- Sportiva/src/pages/StudentsListing/StudentsCard.jsx
+++ Sportiva/src/pages/StudentsListing/StudentsCard.jsx
@@ -1,26 +1,28 @@
 import { useEffect, useState } from "react";
 import { Box, Flex, Image, Text } from "@chakra-ui/react";
 import ToolBar from "../../components/StudentsListingPage/ToolBar";
-// import data from "../../components/StudentsListingPage/Data";
 
 export default function StudentsCard() {
   const [filteredData, setFilteredData] = useState([]);
+  const [searchedData, setSearchedData] = useState([]);
+
   useEffect(() => {
     async function fetchData() {
       const result = await fetch("http://localhost:4006/api/Students");
       const data = await result.json();
-      setFilteredData(data)
+      setFilteredData(data);
     }
-    fetchData()
-  },[]);
+    fetchData();
+  }, []);
 
   const handleSearch = (e) => {
     const keyword = e.target.value.toLowerCase();
     const filtered = filteredData.filter((student) =>
       student.name.toLowerCase().includes(keyword)
     );
-    setFilteredData(filtered)
+  setSearchedData(filtered);
   };
+
   return (
     <>
       <ToolBar handleSearch={handleSearch} />
@@ -30,39 +32,42 @@ export default function StudentsCard() {
           gridTemplateColumns="repeat(5, minmax(0, 1fr))"
           gap="35px"
         >
-          {filteredData.map((student) => (
-            <Box
-              key={student.id}
-              bg="white"
-              border="1px"
-              borderColor="#C7C7C7"
-              w="240px"
-              h="226px"
-              transition="all 0.1s ease-in-out"
-              _hover={{
-                boxShadow: "0px 0px 30px rgba(0, 0, 0, 0.1)",
-                transform: "scale(1.12)"
-              }}
-            >
-              <Image
-                src={student.image}
-                alt={student.name}
-                h="124px"
-                w="205px"
-                ml="18px"
-                pt="15px"
-              />
-              <Text fontWeight="bold" ml="18px" pt="15px">
-                {student.name}
-              </Text>
-              <Text pt="15px" ml="18px">
-                {student.belt_grade}
-              </Text>
-            </Box>
-          ))}
+          {searchedData.length > 0 ? (
+            searchedData.map((student) => (
+              <Box
+                key={student.id}
+                bg="white"
+                border="1px"
+                borderColor="#C7C7C7"
+                w="240px"
+                h="226px"
+                transition="all 0.1s ease-in-out"
+                _hover={{
+                  boxShadow: "0px 0px 30px rgba(0, 0, 0, 0.1)",
+                  transform: "scale(1.12)",
+                }}
+              >
+                <Image
+                  src={student.image}
+                  alt={student.name}
+                  h="124px"
+                  w="205px"
+                  ml="18px"
+                  pt="15px"
+                />
+                <Text fontWeight="bold" ml="18px" pt="15px">
+                  {student.name}
+                </Text>
+                <Text pt="15px" ml="18px">
+                  {student.belt_grade}
+                </Text>
+              </Box>
+            ))
+          ) : (
+            <></>
+          )}
         </Box>
       </Flex>
     </>
   );
 }
-
