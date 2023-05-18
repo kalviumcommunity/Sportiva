69e8faafcc3bcd9cef590cd6a2af041e800a95f1
Harish <harishnik08@gmail.com>
2023-05-18 16:17:26 +0530

    created post api's

--- Backend/server/server.js
+++ Backend/server/server.js
@@ -1,12 +1,15 @@
 const data = require("../../Sportiva/src/components/StudentsListingPage/Data.json");
 const express = require("express");
 const cors = require("cors");
+const datapath = "../../Sportiva/src/components/StudentsListingPage/Data.json";
+const fs = require("fs")
 
 const app = express();
 const port = 4006;
 
 // Enable CORS
 app.use(cors());
+app.use(express.json());
 
 app.get("/api/Students", (req, res) => {
   res.json(data);
@@ -18,12 +21,58 @@ app.get("/api/Students/:id", async(req, res) => {
   res.json(filteredStudents);
 });
 
-// app.post("/api/Students", (req, res) => {
-//   const newStudent = req.body;
-//   newStudent.id = data.length.toString();
-//   data.push(newStudent);
-//   res.status(201).json(newStudent);
-// });
+
+
+app.post("/api/Students/form", (req, res) => {
+  console.log(req.body)
+  const { id, image, name, belt_grade, years_of_exp, coach_notes } = req.body;
+  const newStudent = { id, image, name, belt_grade, years_of_exp, coach_notes };
+
+  let studentdata=[];
+  try{
+    studentdata = require(datapath)
+  } catch (err) {
+    console.error("Error reading student data.json" , err );
+    return res.status(500).json({message:"Internal server error"})
+  }
+  studentdata.push(newStudent);
+  
+  fs.writeFile(datapath, JSON.stringify(studentdata, null, 2), (err) => {
+    if (err) {
+      console.error("Error writing data.json", err);
+      return res.status(500).json({ message: "Internal server error" });
+    } else {
+      res.json({ message: "Student added sucessfully" });
+    }
+  });
+
+});
+
+app.post("/api/Students/studentAnalytics/modal", (req, res) => {
+  console.log(req.body);
+  const { coach_id , date , note , skills } = req.body;
+  const newSession = { coach_id, date, note, skills };
+
+  let sessiondata = [];
+  try {
+    sessiondata = require(datapath);
+  } catch (err) {
+    console.error("Error reading student data.json", err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+  sessiondata.push(newSession);
+
+  fs.writeFile(datapath, JSON.stringify(sessiondata, null, 2), (err) => {
+    if (err) {
+      console.error("Error writing data.json", err);
+      return res.status(500).json({ message: "Internal server error" });
+    } else {
+      res.json({ message: "Student added sucessfully" });
+    }
+  });
+});
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
